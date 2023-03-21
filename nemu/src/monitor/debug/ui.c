6c1317cc89db5ae2ff6944a98fcffe511d0c8c14
OYO-OZO <2234287549@qq.com>
2023-03-21 14:30:52 +0800

    ui.c half

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -36,7 +36,57 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int cmd_p(char* args){
+    bool success = false;
+    int val = expr(args, &success);
+    if(success){
+        printf("result %d\n",val);
+    } else{
+        printf("expr error");
+    }
+    return 0;
+}
+
+static int cmd_si(char* args){
+    uint64_t n = 1;
+    if(args != NULL){
+        int si_suc = sscanf(args,"%llu",&n);
+        if(si_suc <= 0 ){
+            printf("args error in cmd_si\n");
+            return 0;
+        }
+    }
+    cpu_exec(n);
+    return 0;
+}
+
+static int cmd_d(char* args){
+    //删 NO 监视点
+    int NO;
+    int NO_ret = sscanf(args, "%d", &NO);
+
+    if(NO_ret<=0){
+        printf("args wp_si_error\n");
+        return 0;
+    }
+
+    free_wp(NO);
+
+    return 0;
+}
+
+static int cmd_w(char* args){
+    //set监视点
+    new_wp(args);
+    return 0;
+}
+
+//先声明
+static int cmd_x(char* args);
 static int cmd_help(char *args);
+static int cmd_info(char* args);
+
+
 
 static struct {
   char *name;
@@ -46,8 +96,13 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
-  /* TODO: Add more commands */
+  //Add more commands
+  { "si", "execute [N] instructions", cmd_si},
+  { "info", "[-r] reg info, [-w] watchpoint info", cmd_info},
+  { "p","expr_val",cmd_p},
+  { "x", "[N] [expr] memory ", cmd_x},
+  { "w", "[expr] watchpoint", cmd_w},
+  { "d", "[N] delete the N watchpoint", cmd_d}
 
 };
 
@@ -74,6 +129,48 @@ static int cmd_help(char *args) {
     printf("Unknown command '%s'\n", arg);
   }
   return 0;
+}
+
+static int cmd_x(char* args){
+    if(args == NULL){
+        printf("too few parameter! \n");
+        return 0;
+    }
+
+    char *arg = strtok(args," ");
+    if(arg == NULL){
+        printf("too few parameter! \n");
+        return 0;
+    }
+    int num ;
+    sscanf(arg, "%d", &num);
+    char *expr_args = strtok(NULL," ");
+    if(expr_args == NULL){       // 找不到地址参数
+        printf("too few parameter! \n");
+        return 0;
+    }
+    if(strtok(NULL," ")!=NULL){  // 还能切出来参数
+        printf("too many parameter! \n");
+        return 0;
+    }
+
+    bool success = true;
+    vaddr_t addr = expr(expr_args , &success);
+    if (success!=true){
+        printf("ERRO!!\n");
+        return 1;
+    }
+
+    printf("memory:\n");
+    for(int i = 0 ; i < num ; i++){
+
+        printf("0x%08x:  ", addr + i*4 );
+        for(int j =0 ; j < 4 ; j++){
+            printf("0x%02x " , vaddr_read(addr + i * 4 + j,1 ));
+        }
+        printf("\n");
+    }
+    return 0;
 }
 
 void ui_mainloop(int is_batch_mode) {
